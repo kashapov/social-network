fd31a9cdeb6a8e37ffa9d5c9e8173f1a1c449cec
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-02 19:40:47 +0300

    refactor: Users component to class

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,64 +5,69 @@ import * as axios from "axios";
 import classes from "./Users.module.css";
 import userPhoto from "../../assets/images/user-avatar.jpg";
 
-const Users = props => {
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-          props.setUsers(response.data.items);
-        });
-    }
+class Users extends React.PureComponent {
+  getUsers = () => {
+    const { setUsers } = this.props;
+
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        setUsers(response.data.items);
+      });
   };
 
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {props.users.map(u => (
-        <div className={classes.userBlock} key={u.id}>
-          <div className={classes.userBlockLeft}>
-            <div>
-              <img
-                className={classes.userPhoto}
-                src={u.photos.small ? u.photos.small : userPhoto}
-                alt={u.name}
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  follow
-                </button>
-              )}
-            </div>
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    const { users, follow, unfollow } = this.props;
+
+    const usersList = users.map(u => (
+      <div className={classes.userBlock} key={u.id}>
+        <div className={classes.userBlockLeft}>
+          <div>
+            <img
+              className={classes.userPhoto}
+              src={u.photos.small ? u.photos.small : userPhoto}
+              alt={u.name}
+            />
           </div>
-          <div className={classes.userBlockRight}>
-            <div>
-              <div className={classes.userName}>{u.name}</div>
-              <div className={classes.userStatus}>Status: {u.status}</div>
-            </div>
-            <div className={classes.userLocation}>
-              <div>Country: ...</div>
-              <div>City: ...</div>
-            </div>
+          <div>
+            {u.followed ? (
+              <button
+                onClick={() => {
+                  unfollow(u.id);
+                }}
+              >
+                Unfollow
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  follow(u.id);
+                }}
+              >
+                follow
+              </button>
+            )}
           </div>
         </div>
-      ))}
-    </div>
-  );
-};
+        <div className={classes.userBlockRight}>
+          <div>
+            <div className={classes.userName}>{u.name}</div>
+            <div className={classes.userStatus}>Status: {u.status}</div>
+          </div>
+          <div className={classes.userLocation}>
+            <div>Country: ...</div>
+            <div>City: ...</div>
+          </div>
+        </div>
+      </div>
+    ));
+
+    return <div>{usersList}</div>;
+  }
+}
 
 export default Users;
