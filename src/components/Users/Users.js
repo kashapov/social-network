61af3262c7cf9bbd53da9652d5aaa6c47c1e2fd2
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-02 17:47:10 +0300

    feat: get users data from API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,56 +1,33 @@
 import React from "react";
 
+import * as axios from "axios";
+
 import classes from "./Users.module.css";
+import userPhoto from "../../assets/images/user-avatar.jpg";
 
 const Users = props => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          "http://www.clker.com/cliparts/7/8/2/2/135162274923698324Mini%20Avatar.svg.med.png",
-        fullName: "Andrey Kashapov",
-        status: "In Musk We Trust",
-        location: {
-          country: "Ukraine",
-          city: "Lviv"
-        },
-        followed: true
-      },
-      {
-        id: 2,
-        photoUrl:
-          "http://www.clker.com/cliparts/7/8/2/2/135162274923698324Mini%20Avatar.svg.med.png",
-        fullName: "Elena Zamnius",
-        status: "status",
-        location: {
-          country: "Ukraine",
-          city: "Harkov"
-        },
-        followed: false
-      },
-      {
-        id: 3,
-        photoUrl:
-          "http://www.clker.com/cliparts/7/8/2/2/135162274923698324Mini%20Avatar.svg.med.png",
-        fullName: "Nataly Matveeva",
-        status: "status",
-        location: {
-          country: "Belarus",
-          city: "Minsk"
-        },
-        followed: false
-      }
-    ]);
-  }
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {props.users.map(u => (
         <div className={classes.userBlock} key={u.id}>
           <div className={classes.userBlockLeft}>
             <div>
-              <img className={classes.userPhoto} src={u.photoUrl} alt="user" />
+              <img
+                className={classes.userPhoto}
+                src={u.photos.small ? u.photos.small : userPhoto}
+                alt={u.name}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -74,12 +51,12 @@ const Users = props => {
           </div>
           <div className={classes.userBlockRight}>
             <div>
-              <div>{u.fullName}</div>
+              <div className={classes.userName}>{u.name}</div>
               <div className={classes.userStatus}>Status: {u.status}</div>
             </div>
             <div className={classes.userLocation}>
-              <div>Contry: {u.location.country}</div>
-              <div>City: {u.location.city}</div>
+              <div>Country: ...</div>
+              <div>City: ...</div>
             </div>
           </div>
         </div>
