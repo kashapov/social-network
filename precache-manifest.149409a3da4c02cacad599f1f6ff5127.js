d93eeabaafdbcad582e3dbd53f894648d2f6efd4
Andrey Kashapov <kashapovandrey@gmail.com>
2019-10-26 20:29:32 +0300

    Updates

---
+++ precache-manifest.149409a3da4c02cacad599f1f6ff5127.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049c9fbca9f50cab6905cf7a562c97f7",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "6925e989ec094a88007a",
+    "url": "/social-network/static/css/3.ba80911e.chunk.css"
+  },
+  {
+    "revision": "4ac0e658ebeb2b789a25",
+    "url": "/social-network/static/css/4.656dd1de.chunk.css"
+  },
+  {
+    "revision": "5900ffd1566a32c01a9e",
+    "url": "/social-network/static/css/5.e70fa240.chunk.css"
+  },
+  {
+    "revision": "87680eebcea5041d159f",
+    "url": "/social-network/static/css/main.a1a847bb.chunk.css"
+  },
+  {
+    "revision": "4808461939659bd07f0d",
+    "url": "/social-network/static/js/2.ec686714.chunk.js"
+  },
+  {
+    "revision": "6925e989ec094a88007a",
+    "url": "/social-network/static/js/3.a66705a4.chunk.js"
+  },
+  {
+    "revision": "4ac0e658ebeb2b789a25",
+    "url": "/social-network/static/js/4.ff51272b.chunk.js"
+  },
+  {
+    "revision": "5900ffd1566a32c01a9e",
+    "url": "/social-network/static/js/5.aea5ece2.chunk.js"
+  },
+  {
+    "revision": "87680eebcea5041d159f",
+    "url": "/social-network/static/js/main.fd2a4192.chunk.js"
+  },
+  {
+    "revision": "bfa0b931d8ff94d493a6",
+    "url": "/social-network/static/js/runtime-main.58dd86ca.js"
+  },
+  {
+    "revision": "acf8252599df9c602a39a8602441fd60",
+    "url": "/social-network/static/media/spinner.acf82525.svg"
+  },
+  {
+    "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
+    "url": "/social-network/static/media/user-avatar.c7b5f6a0.jpg"
+  }
+]);
